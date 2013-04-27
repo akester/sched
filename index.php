3f1378fab05caceec9b8127b4e0d2e3989f8925e
Andrew Kester <akester@cox.net>
2013-04-26 19:53:51 -0500

    www: including authentication and main class

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
+<?php 
+	require 'include/php-digest-mysql.class.php';
+	$auth = new phpAuthMySQL();
+	$auth->auth(true);
+	
+	require 'include/sched/sched.class.php';
+	$ms = new sched_Sched();
+?>
 <html>
 	<head>
 		<title>Machine Scheduling</title>
+		<link rel="stylesheet" type="text/css" href="css/main.css" />
 	</head>
 	<body>
-		<h1>Hello World!</h1>
 	</body>
 </html>
