21c4eaba324ffebca0fbef257edf7cf876a994e5
Andrew Kester <akester@cox.net>
2013-04-27 11:04:47 -0500

    js: adding function to focus text areas that are not complete

--- js/sched/sched_jobFormValidation.js
+++ js/sched/sched_jobFormValidation.js
@@ -10,34 +10,42 @@ function sched_validateJobForm() {
 	
 	if (jobId==null || jobId=="") {
 		alert("Job ID is required.");
+		$('[name=jobId]').focus();
 		return false;
 	}
 	if (machine==null || machine=="") {
 		alert("Machine is required.");
+		$('[name=machine]').focus();
 	  	return false;
 	}
 	if (hours==null || hours=="") {
 		alert("Total Hours is required.");
+		$('[name=hours]').focus();
 		return false;
 	}
 	if (hoursToGo==null || hoursToGo=="") {
 		alert("Hours Remain is required.");
+		$('[name=hoursToGo]').focus();
 		return false;
 	}
 	if (partNo==null || partNo=="") {
 		alert("Part Number is required.");
+		$('[name=partNo]').focus();
 		return false;
 	}
 	if (material==null || material=="") {
 		alert("Material is required.");
+		$('[name=material]').focus();
 		return false;
 	}
 	if (qtyRemain==null || qtyRemain=="") {
 		alert("Remaining Qty is required.");
+		$('[name=qtyRemain]').focus();
 		return false;
 	}
 	if (due==null || due=="") {
 		alert("Date Due is required");
+		$('[name=due]').focus();
 		return false;
 	}
 	
