238f16932a52a15e9ba8e41b2f5fd5bcb8323a2d
Andrew Kester <akester@cox.net>
2013-04-28 17:17:54 -0500

    www: updating authentication

--- ajax.php
+++ ajax.php
@@ -1,7 +1,7 @@
 <?php
-	#require 'include/php-digest-mysql.class.php';
-	#$auth = new phpAuthMySQL();
-	#$auth->auth(true);
+	require 'include/php-digest-mysql.class.php';
+	$auth = new phpAuthMySQL();
+	$auth->auth(true);
 
 	require 'include/sched/sched_ajax.class.php';
 	$ajax = new sched_ajax();
