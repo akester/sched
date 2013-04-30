14319e00c9b0735fb103e985263e2a0258362221
Andrew Kester <akester@cox.net>
2013-04-30 17:40:57 -0500

    www: adding timescale to machine grid

--- js/sched_grid.js
+++ js/sched_grid.js
@@ -1,9 +1,32 @@
 function sched_grid(scale) {
+	var dateBox = new Array();
+	if (scale == 1) {
+		dateBox['number'] = 24;
+		dateBox['unit'] = 'h';
+	}
+	if (scale >= 2 && scale <= 14) {
+		dateBox['number'] = scale;
+		dateBox['unit'] = 'd';
+	}
+	if (scale >= 15) {
+		dateBox['number'] = Math.floor(scale / 7);
+		dateBox['unit'] = 'w';
+	}
+	var pct = Math.floor(100 / dateBox['number']);
 	$.getJSON('ajax/getGridData.php', {
         version: '1',
         s: scale
     }, function(data) {
-    	var tblData = "<tr><th>Machine</th><th>Time</th></tr>";
+    	var tblData = "<tr>" +
+    			"<th>Machine</th>" +
+    			"<td>";
+    	var i = 0;
+    	while (i < dateBox['number']) {
+    		tblData += '<span class="jobBox" style="width:' + pct + '%;">' 
+    			+ i + dateBox.unit + '</span>';
+    		i++;
+    	};
+    	tblData += '</td></tr>';
     	$.each(data, function(machine, grid){
     		tblData += '<tr>';
     		tblData += '<td><a href="viewMachine.php?m=' + machine + '">' + machine + '</td>';
